f0df82b578bb3789289a778a729a68b644ad2fdc
Josip Lausic <lausic99@gmail.com>
2023-07-13 20:11:33 +0200

    Backend initialization

---
+++ oglasnik-backend/src/main/java/oss/zavrsni/oglasnikbackend/OglasnikBackendApplication.java
@@ -0,0 +1,13 @@
+package oss.zavrsni.oglasnikbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OglasnikBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OglasnikBackendApplication.class, args);
+	}
+
+}
---
+++ oglasnik-backend/src/test/java/oss/zavrsni/oglasnikbackend/OglasnikBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package oss.zavrsni.oglasnikbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OglasnikBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
